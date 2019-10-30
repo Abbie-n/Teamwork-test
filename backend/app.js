e601aa32a200eb68333aca69a22f4b9579161a06
Abbie <abbie@github.com>
2019-10-30 18:21:03 +0100

    Added connection to postgres

---
+++ backend/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const {Pool, Client} = require('pg');
+
+app = express();
+const connection = "postgres://AbbieN:confessor@localhost:5432/Teamworkdb";
+
+const client = new Client({
+    connectionString: connection
+})
+
+client.connect()
+.then(() => {
+    console.log('Connection succesful!')
+}).catch (() => {
+    console.log('Unable to connect!')
+    console.error(error);
+});
+
+module.exports = app;
